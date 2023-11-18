5be425177c28c87abefbc9ca2e8f3795df125bd8
xpnsive03 <marynelle.aban1997@gmail.com>
2023-11-18 11:47:19 +0800

    v2

---
+++ Commons.v2/IAttributes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrambled_Word_WPF_Project.Commons.v2
+{
+    public interface IAttributes
+    {
+        int WORD { get; set; }
+        int HINT { get; set; }
+    }
+}
--- Commons/Attributes.cs
+++ Commons/Attributes.cs
@@ -6,15 +6,11 @@ using System.Threading.Tasks;
 
 namespace Scrambled_Word_WPF_Project.Commons
 {
-    public class Attributes
+    public class EasyWordAttributes
     {
         public string WORD { get; set; }
         public string HINT { get; set; }
-        public Attributes(int iD, string wORD, string hINT)
-        {
-            WORD = wORD;
-            HINT = hINT;
-        }
+
 
     }
 }
